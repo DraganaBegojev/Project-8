6a7ddd67c1a37f599d719d7562f50869603dc09c
DraganaBegojev <163642152+DraganaBegojev@users.noreply.github.com>
2024-11-18 07:32:35 +0100

    Display Employees JS
    function displayEmployees

--- js/app.js
+++ js/app.js
@@ -6,3 +6,43 @@ const gridContainer = document.querySelector(".grid-container");
 const overlay = document.querySelector(".overlay");
 const modalContainer = document.querySelector(".modal-content");
 const modalClose = document.querySelector(".modal-close");
+
+// fetch data from API
+
+fetch(urlAPI)
+    .then(res => {
+        if (!res.ok) {
+            throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        return res.json();
+    })
+    .then(res => {
+        console.log(res); // Log full response to examine its structure
+        return res.results; // Extract and return the "results" array
+    })
+    .then(displayEmployees) // Pass data to the displayEmployees function
+    .catch(err => console.error("Error fetching data:", err));
+
+// Function to display employees
+
+function displayEmployees(employeeData) {
+    employees = employeeData;
+    let employeeHTML = '';
+    employees.forEach((employee, index) => {
+        let name = employee.name;
+        let email = employee.email;
+        let city = employee.location.city;
+        let picture = employee.picture;
+    employeeHTML += `
+        <div class="card" data-index="${index}">
+            <img class="avatar" src="${picture.large}" />
+            <div class="text-container">
+                <h2 class="name">${name.first} ${name.last}</h2>
+                <p class="email">${email}</p>
+                <p class="address">${city}</p>
+            </div>
+        </div>
+    `
+});
+    gridContainer.innerHTML = employeeHTML;
+}
